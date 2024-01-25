b592bf0e68af985de33c7508d868ca296d084a5e
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-01-26 02:56:04 +0300

    feat: add create files

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import { writeFile } from "node:fs/promises";
+import { getPathToFile } from "../utils/pathToFile.js";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    const fileUrl = getPathToFile(import.meta.url, "files", "fresh.txt");
+    const data = "I am fresh and young";
+    await writeFile(fileUrl, data);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await create();
