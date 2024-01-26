dff79f731363984d73818ad7a6843087a11bb9f3
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-01-26 03:45:54 +0300

    feat: add delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import { unlink } from "node:fs/promises";
+import { getPathToFile } from "../utils/pathToFile.js";
+import { checkFileExist } from "../utils/checkFileExist.js";
+
 const remove = async () => {
-    // Write your code here 
+  const initialFile = getPathToFile(
+    import.meta.url,
+    "files",
+    "fileToRemove.txt"
+  );
+
+  try {
+    if (!(await checkFileExist(initialFile))) {
+      throw new Error("FS operation failed");
+    }
+
+    await unlink(initialFile);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await remove();
