9b1afc0f39b9b8a027177b2ad8bb8ddb4d44b577
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-01-26 04:55:09 +0300

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createReadStream } from "node:fs";
+import { createHash } from "node:crypto";
+import { getPathToFile } from "../utils/pathToFile.js";
 
+const calculateHash = async () => {
+  const file = getPathToFile(
+    import.meta.url,
+    "files",
+    "fileToCalculateHashFor.txt"
+  );
+
+  const hash = createHash("sha256");
+
+  const input = createReadStream(file);
+  input.on("readable", () => {
+    const data = input.read();
+    if (data) hash.update(data);
+    else {
+      console.log(hash.digest("hex"));
+    }
+  });
+};
 await calculateHash();
