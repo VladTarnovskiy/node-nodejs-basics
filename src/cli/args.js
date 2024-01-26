864c9400f55b43e1a0bcdb7d975e7cc0513feec4
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-01-26 04:19:13 +0300

    feat: add args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  try {
+    const argvList = process.argv;
+    argvList.forEach((arg, index) => {
+      if (arg.includes("--")) {
+        console.log(`${arg.slice(2)} is ${argvList[index + 1]}`);
+      }
+    });
+    console.log(argvList);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,9 +1,13 @@
 const parseEnv = () => {
-  const envs = process.env;
-  for (let key in envs) {
-    if (key.match(/RSS_/)) {
-      console.log(`${key}=${envs[key]}`);
+  try {
+    const envs = process.env;
+    for (let key in envs) {
+      if (key.match(/RSS_/)) {
+        console.log(`${key}=${envs[key]}`);
+      }
     }
+  } catch (error) {
+    console.log(error.message);
   }
 };
 
