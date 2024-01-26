67aec612f25b87329c08bb0663c9cbb575f9ebdf
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-01-26 04:01:29 +0300

    feat: add list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { readdir } from "node:fs/promises";
+import { getPathToFile } from "../utils/pathToFile.js";
+import { checkFileExist } from "../utils/checkFileExist.js";
+
 const list = async () => {
-    // Write your code here 
+  const initialFile = getPathToFile(import.meta.url, "files", "");
+
+  try {
+    if (!(await checkFileExist(initialFile))) {
+      throw new Error("FS operation failed");
+    }
+
+    const files = await readdir(initialFile);
+    files.forEach((file) => console.log(file));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await list();
